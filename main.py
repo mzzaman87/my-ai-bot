e60bbddf590949f0ac73eba4b0e24ffba7698bd1
mzzaman87 <mzzaman87@gmail.com>
2026-06-27 22:20:16 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# ================= HOME =================
 @app.get("/")
 def home():
-    return {"status": "MonirBot reset started"}
+    return {"status": "MonirBot running clean version"}
 
-
+# ================= WEBHOOK VERIFY =================
 @app.get("/webhook")
 def verify():
     return "MonirBot webhook active"
